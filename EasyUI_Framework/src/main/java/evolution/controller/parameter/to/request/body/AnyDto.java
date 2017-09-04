7710ce96c81aa08a0ba2e3e0b248e7c0badc7793
chenli <chenli@192.168.43.24>
2017-09-04 18:50:11 +0800

    new demos

---
+++ EasyUI_Framework/src/main/java/evolution/controller/parameter/to/request/body/AnyDto.java
@@ -0,0 +1,22 @@
+package evolution.controller.parameter.to.request.body;
+
+public class AnyDto {
+	private String name;
+	private int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "AnyDto [name=" + name + ", age=" + age + "]";
+	}
+}
